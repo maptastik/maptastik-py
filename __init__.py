0d385f320cc1bf402f63121fc0ab50e5a311674b
Ryan Cooper <ryancooper729@gmail.com>
2017-10-09 14:30:47 -0400

    add field info function

--- __init__.py
+++ __init__.py
@@ -9,6 +9,13 @@ import arcpy
 import statistics
 
 def basic_fc_stats(fc, field):
+	"""Return a dictionary of basic summary statistics for a field.
+
+	Keyword arguments:
+	fc -- Feature class containing the field to summarize
+	field -- Field in feature class to summarize
+	"""
+
 	field_vals = []
 	total = 0
 	row_count = 0
@@ -22,3 +29,27 @@ def basic_fc_stats(fc, field):
 	stdev = statistics.stdev(field_vals)
 	summary = {'sum': total, 'row_count': row_count, 'mean': mean, 'median': median, 'stdev': stdev}
 	return summary
+
+def layer_fields(fc, wild_card=None, field_type='All', field_info=False):
+	"""Return a dictionary of information about fields in a feature class.
+
+	Keyword arguments:
+	fc -- Feature class to get field information about
+	wild_card -- Limits the results returned (Default: None)
+	field_type -- Specified type of field is returned (Default: 'All')
+	field_info -- Prints returned fields with some info about them
+	"""
+	fields = arcpy.ListFields(fc, wild_card, field_type)
+	field_names = [x.name for x in fields]
+	if field_info == True:
+		print("FIELDS")
+		print("-----")
+		for field in fields:
+			print("""
+Field:	{0}
+Type:	{1}
+Editable:	{2}
+Domain:	{3}
+-----
+			""".format(field.name, field.type, field.editable, field.domain))
+	return {"field_names": field_names, "field_amt": len(fields)}
